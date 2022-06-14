b4bcaf7df2d3b00794464cac515eb6015e93704f
NikolayKadiev <82579213+NikolayKadiev@users.noreply.github.com>
2022-06-14 17:49:24 +0300

    Update mcp4725.c

--- mcp4725/mcp4725.c
+++ mcp4725/mcp4725.c
@@ -2,19 +2,27 @@
 
 void main(void) {
     uint8_t data_hi = 0, data_lo = 0;
-    uint16_t set_val = 100;
+    uint16_t set_val = 0, dac_val = 0;
     char msg_lcd[10];
     
-    TRISA=0x01;
+    TRISA=0x03;
     TRISB=0x00;
     TRISC=0x00;
     
+    adc_set();
     i2c_master(100000);
     lcd_set();
     
     __delay_ms(100);
     
     while(1){
+        adc_ch(0);
+        set_val = adc_read();
+        
+        sprintf(msg_lcd, "adc_in = %d \n", set_val);
+        lcd_adr(0,0);
+        lcd_sendstr(msg_lcd);
+        
         // For fast mode
         data_lo = set_val & 0xFF;
         data_hi = set_val >> 8;
@@ -24,13 +32,17 @@ void main(void) {
 		i2c_write(data_hi);
         i2c_write(data_lo);
         i2c_stop();
+        __delay_ms(1);
         
-        sprintf(msg_lcd, "int out = %d \n", set_val);
-        lcd_adr(0,0);
+        
+        adc_ch(1);
+        dac_val = adc_read();
+        sprintf(msg_lcd, "dac_out = %d \n", dac_val);
+        lcd_adr(1,0);
         lcd_sendstr(msg_lcd);
-        __delay_ms(100);
         __delay_ms(100);
     }
     
     return;
 }
+

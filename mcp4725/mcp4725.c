8490509b034dac9de6716df3e94480100a7b0cf5
NikolayKadiev <82579213+NikolayKadiev@users.noreply.github.com>
2022-06-24 18:50:48 +0300

    Add files via upload

--- mcp4725/mcp4725.c
+++ mcp4725/mcp4725.c
@@ -3,7 +3,7 @@
 void main(void) {
     uint8_t data_hi = 0, data_lo = 0;
     uint16_t set_val = 0, dac_val = 0;
-    char msg_lcd[10];
+    char msg_lcd[20];
     
     TRISA=0x03;
     TRISB=0x00;
@@ -32,7 +32,7 @@ void main(void) {
 		i2c_write(data_hi);
         i2c_write(data_lo);
         i2c_stop();
-        __delay_ms(1);
+        __delay_ms(10);
         
         
         adc_ch(1);
@@ -40,9 +40,8 @@ void main(void) {
         sprintf(msg_lcd, "dac_out = %d \n", dac_val);
         lcd_adr(1,0);
         lcd_sendstr(msg_lcd);
-        __delay_ms(100);
+        __delay_ms(10);
     }
     
     return;
 }
-

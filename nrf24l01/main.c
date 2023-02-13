fd5c3369b453d3fd707cd3f7ed313cc20b48bd15
NikolayKadiev <82579213+NikolayKadiev@users.noreply.github.com>
2023-02-13 15:41:52 +0200

    Update main.c

--- nrf24l01/main.c
+++ nrf24l01/main.c
@@ -15,7 +15,7 @@
 #define SPI_TX_PIN  11
 #define SPI_SCK_PIN 10
 #define SPI_CSN_PIN	13
-#define PIN_CE		14
+#define PIN_CE		9
 #define PIN_DRDY    8
 
 #define led_pin     25
@@ -24,6 +24,28 @@ uint8_t status = 0, data_in = 0;
 void gpio_callback(uint gpio, uint32_t events) {
     status = gpio;
     return;
+}
+
+void pins_init(void){
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+  
+    gpio_init(PIN_CE);
+    gpio_set_dir(PIN_CE, GPIO_OUT);
+	gpio_put(PIN_CE, 0);
+
+    gpio_init(PIN_DRDY);
+    gpio_set_dir(PIN_DRDY, GPIO_IN);
+     
+    spi_init(spi1, 500 * 1000);
+    spi_set_format(spi1, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+    gpio_set_function(SPI_RX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_TX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_CSN_PIN, GPIO_FUNC_SPI);
+    bi_decl(bi_4pins_with_func(SPI_RX_PIN, SPI_TX_PIN, SPI_SCK_PIN, SPI_CSN_PIN, GPIO_FUNC_SPI));
+
+    gpio_put(led_pin, 0);
 }
 
 void main(void){
@@ -55,27 +77,9 @@ void main(void){
 	};
 
     board_init();
-
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
-  
-    gpio_init(PIN_CE);
-    gpio_set_dir(PIN_CE, GPIO_OUT);
-	gpio_put(PIN_CE, 0);
-
-    gpio_init(PIN_DRDY);
-    gpio_set_dir(PIN_DRDY, GPIO_IN);
-     
-    spi_init(spi1, 500 * 1000);
-    spi_set_format(spi1, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
-    gpio_set_function(SPI_RX_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(SPI_SCK_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(SPI_TX_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(SPI_CSN_PIN, GPIO_FUNC_SPI);
-    bi_decl(bi_4pins_with_func(SPI_RX_PIN, SPI_TX_PIN, SPI_SCK_PIN, SPI_CSN_PIN, GPIO_FUNC_SPI));
-
-    gpio_put(led_pin, 0);
-
+    tusb_init(); 
+	pins_init();
+	
     gpio_set_irq_enabled_with_callback(PIN_DRDY, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
 	gpio_put(PIN_CE, 1);

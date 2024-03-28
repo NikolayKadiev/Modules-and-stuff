5785b10c2d0fccd30705c9d8a2dcad620df1184e
NikolayKadiev <82579213+NikolayKadiev@users.noreply.github.com>
2024-03-28 13:47:45 +0200

    Update mma8452.h

--- mma8452/esp32_mma8452/mma8452.h
+++ mma8452/esp32_mma8452/mma8452.h
@@ -4,29 +4,29 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
+
 #include "driver/i2c.h"
 
-const uint8_t ADDRESS = 0x1C<<1;
+#define MMA8452_ADDR            0x1C<<1
 
 //hardware registers
 
-const uint8_t REG_X_MSB = 0x01;
-const uint8_t REG_X_LSB = 0x02;
-const uint8_t REG_Y_MSB = 0x03;
-const uint8_t REG_Y_LSB = 0x04;
-const uint8_t REG_Z_MSB = 0x05;
-const uint8_t REG_Z_LSB = 0x06;
-const uint8_t REG_DATA_CFG = 0x0E;
-const uint8_t REG_CTRL_REG1 = 0x2A;
-const uint8_t REG_CTRL_REG2 = 0x2B;
-const uint8_t REG_CTRL_REG3 = 0x2C;
-const uint8_t REG_CTRL_REG4 = 0x2D;
-const uint8_t REG_CTRL_REG5 = 0x2E;
+#define REG_X_MSB       0x01
+#define REG_X_LSB       0x02
+#define REG_Y_MSB       0x03
+#define REG_Y_LSB       0x04
+#define REG_Z_MSB       0x05
+#define REG_Z_LSB       0x06
+#define REG_DATA_CFG    0x0E
+#define REG_CTRL_REG1   0x2A
+#define REG_CTRL_REG2   0x2B
+#define REG_CTRL_REG3   0x2C
+#define REG_CTRL_REG4   0x2D
+#define REG_CTRL_REG5   0x2E
 
 // Set the range and precision for the data 
-const uint8_t range_config = 0x00; // 0x00 for ±2g, 0x01 for ±4g, 0x02 for ±8g
-const float count = 4096; // 4096 for ±2g, 2048 for ±4g, 1024 for ±8g
+#define range_config    0x00 // 0x00 for ±2g, 0x01 for ±4g, 0x02 for ±8g
+#define count           4096 // 4096 for ±2g, 2048 for ±4g, 1024 for ±8g
 
 float mma8452_convert_accel(uint16_t raw_acc);
 void mma8452_write_reg(uint8_t reg_addr, uint8_t data);

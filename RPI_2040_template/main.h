45edc6e3009867bf25ae98e4ff2f25b22c927054
NikolayKadiev <82579213+NikolayKadiev@users.noreply.github.com>
2024-11-20 07:48:31 +0200

    Add files via upload

---
+++ RPI_2040_template/main.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "bsp/board.h"
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "tusb.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include "hardware/i2c.h"
+
